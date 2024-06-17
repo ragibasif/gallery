aba7b7494572a9336fa8705eca10b265d758910b
Ragib Asif <github.decibel284@passinbox.com>
2024-06-17 00:20:54 -0400

    change favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import ImageGallery from "./components/ImageGallery";
+import ExperimentalGallery from "./components/ExperimentalGallery";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Nav />
+      <Header />
       <h1>Photo Journal</h1>
+      {/* <ExperimentalGallery/> */}
       <ImageGallery />
+      <Footer />
     </>
   );
 }
